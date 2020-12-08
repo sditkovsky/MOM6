fab7c904676f82f0a36eed1ac88f3a8e29271687
Enhui Liao <enhuil@princeton.edu>
2020-12-08 17:26:04 -0500

    turn on cobalt tracer computation

--- src/tracer/MOM_generic_tracer.F90
+++ src/tracer/MOM_generic_tracer.F90
@@ -201,7 +201,7 @@ contains
        if (g_tracer_is_prog(g_tracer)) then
          call register_tracer(tr_ptr, tr_Reg, param_file, HI, GV, &
                               name=g_tracer_name, longname=longname, units=units, &
-                              registry_diags=.false., &   !### CHANGE TO TRUE?
+                              registry_diags=.true., &   !liao### CHANGE TO TRUE?
                               restart_CS=restart_CS, mandatory=.not.CS%tracers_may_reinit)
        else
          call register_restart_field(tr_ptr, g_tracer_name, .not.CS%tracers_may_reinit, &
